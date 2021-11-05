500ad55c79956bff4cbe28b8a695815cbbef38ef
Acla putra <muhamadacla22@gmail.com>
2021-11-06 01:53:00 +0700

    modified app/core/App.php

--- app/core/App.php
+++ app/core/App.php
@@ -1,14 +1,32 @@
 <?php
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct() {
         $url = $this->parseURL();
-        var_dump($url);
+        
+        if(file_exists('../app/controllers/' . $url[0] . '.php')) {
+            $this->controller = $url[0];
+            // unset($url[0]);
+            var_dump($url);
+            echo 'exists';
+
+        } else {
+            echo 'not exists';
+        }
     }
 
     public function parseURL() {
         if(isset($_GET['url'])) {
-            $url = $_GET['url'];
+            // trim "/" in url
+            $url = rtrim($_GET['url'], '/');
+            // agar url bersih dari karakter aneh
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // url kita pecah dengan explode '/' dari url
+            $url = explode('/', $url);
             return $url;
         }
     }
