f90ef031d63d4c8a6d10fe203c52cd0229b553a6
Acla Putra <muhamadacla22@gmail.com>
2021-11-13 20:32:28 +0700

    get data from url arguments

--- app/controllers/about.php
+++ app/controllers/about.php
@@ -1,8 +1,12 @@
 <?php
 
 class About extends Controller{
-    public function index($nama = 'Acla', $pekerjaan = 'Software engineer', $umur = 21) {
-        $this->view('about/index');
+    public function index($nama = 'Acla', $umur = 21 , $pekerjaan = 'Software engineer') {
+
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $this->view('about/index', $data);
     }
     public function page() {
         $this->view('about/page');
