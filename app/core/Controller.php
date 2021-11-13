23e6955c9394e688d24a617d2941e5855d5a339a
Acla Putra <muhamadacla22@gmail.com>
2021-11-13 15:43:22 +0700

    make a view function to access view pages

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,5 +1,9 @@
 <?php
 
 class Controller {
-    
+    public function view($view, $data = []) {
+        // lokasi default berada di public/index.php
+        $viewPath = "../app/views/";
+        require_once $viewPath .  $view . '.php';
+    }
 }
