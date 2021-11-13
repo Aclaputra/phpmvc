f1b8dfea4a94fe09d416bc034e5d227fc50d8ee0
Acla Putra <muhamadacla22@gmail.com>
2021-11-13 20:31:09 +0700

    modified about/index

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
+    <title>About Page</title>
 </head>
 <body>
-    <h1>About/index</h1>
+    <h1>About Me</h1>
+    <p>Halom nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, saya adalah seorang <?= $data['pekerjaan'] ?>. </p>
 </body>
 </html>
