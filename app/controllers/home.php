91924316f78ea65b23eafc6487916fb6eb4b087f
Acla Putra <muhamadacla22@gmail.com>
2021-11-14 17:55:43 +0700

    make dynamic title in home controller

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -3,6 +3,9 @@
 class Home extends Controller {
     public function index(){
         // panggil function view($view, $data = []) di core/Controller.php 
+        $data['judul'] = 'Home';
+        $this->view('templates/header', $data);
         $this->view('home/index');
+        $this->view('templates/footer');
     }
 }
