0c224884c9dfcc448ddd96bebb75eb41715c5821
Acla Putra <muhamadacla22@gmail.com>
2021-11-15 00:41:25 +0700

    define BASEURL

---
+++ app/core/Constants.php
@@ -0,0 +1,8 @@
+<?php
+
+/** define()
+ * digunakan untuk membuat konstanta 
+ * https://www.php.net/manual/en/function.define.php
+ * define(string $constant_name, mixed $value, bool $case_insensitive = false): bool
+ */
+define('BASEURL','http://localhost/phpmvc/public');
