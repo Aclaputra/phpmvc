bf9b493a03d0fb3d41005e8d5916471385869d67
Acla Putra <muhamadacla22@gmail.com>
2021-11-15 16:59:29 +0700

    made a function model()

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -6,4 +6,15 @@ class Controller {
         $viewPath = "../app/views/";
         require_once $viewPath .  $view . '.php';
     }
+    /** buat function model 
+     * panggil file pada model folders berdasarkan argumen model($model)
+    */
+    public function model($model) {
+        require_once '../app/models/' . $model . '.php';
+        /** new $model
+         * kembalikan nilai dengan instansiasi dahulu dengan new $model- 
+         * karena memanggil class User_model
+         */
+        return new $model;
+    }
 }
