d49d6f28e93e439ff07db4119f9a3552eb04f3bc
Acla Putra <muhamadacla22@gmail.com>
2021-11-16 01:06:23 +0700

    added function detail() get by id

--- app/controllers/mahasiswa.php
+++ app/controllers/mahasiswa.php
@@ -15,4 +15,19 @@ class Mahasiswa extends Controller{
         $this->view('mahasiswa/index', $data);
         $this->view('templates/footer');
     }
+
+    public function detail($id) {
+
+        $data['judul'] = 'Detail Mahasiswa';
+        /**
+         * 1. panggil function model, panggil method di dalamnya 
+         * hasil sudah array ASSOCiative kirim data ke viewnya
+         * menggunakan function view
+         * $this->view('mahasiswa/index', $data);
+         */
+        $data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaById($id);
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/detail', $data);
+        $this->view('templates/footer');
+    }
 }
