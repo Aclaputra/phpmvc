ca9787bf6e9fcf12d68f250f1c06d4215cc0fe64
Acla Putra <muhamadacla22@gmail.com>
2021-11-16 01:06:45 +0700

    moved constants

--- app/core/Constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/** define()
- * digunakan untuk membuat konstanta 
- * https://www.php.net/manual/en/function.define.php
- * define(string $constant_name, mixed $value, bool $case_insensitive = false): bool
- */
-define('BASEURL','http://localhost/phpmvc/public');
