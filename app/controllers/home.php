1963e8d3495c5c49b446bad57379e22cf1a7e0b1
Acla Putra <muhamadacla22@gmail.com>
2021-11-15 16:45:53 +0700

    call function model then call getUser function in User_model

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,10 +2,16 @@
 
 class Home extends Controller {
     public function index(){
+        
+        /** panggil function model setelah itu panggil method di dalamnya yaitu getUser();
+         * Setiap user yang masuk akan dikirimkan ke $data['nama']
+         */
+        $data['nama'] = $this->model('User_model')->getUser();
         // panggil function view($view, $data = []) di core/Controller.php 
         $data['judul'] = 'Home';
         $this->view('templates/header', $data);
-        $this->view('home/index');
+        // kirim $data['nama'] ke home
+        $this->view('home/index', $data);
         $this->view('templates/footer');
     }
 }
