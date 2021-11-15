2732546b55223470c53b613f8730a3870af738e8
Acla Putra <muhamadacla22@gmail.com>
2021-11-16 01:05:45 +0700

    added config & moved constants

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/** define()
+ * digunakan untuk membuat konstanta 
+ * https://www.php.net/manual/en/function.define.php
+ * define(string $constant_name, mixed $value, bool $case_insensitive = false): bool
+ */
+define('BASEURL','http://localhost/phpmvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
+
