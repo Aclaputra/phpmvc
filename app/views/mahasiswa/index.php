78823be2009192dabb3ef6229e52d71cf3239b43
Acla Putra <muhamadacla22@gmail.com>
2021-11-15 20:43:35 +0700

    added form modal to insert new data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,9 @@
 <div class="container mt-4">
     <div class="row">
         <div class="col-6">
+            <button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
             <h3>Daftar Mahasiswa</h3>
             <!-- 2. tampilkan di viewnya
                 dengan menggunakan foreach 
@@ -15,4 +18,49 @@
             <?php endforeach; ?>
         </div>
     </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form action="<?= BASEURL; ?>/mahasiswa/add" method="post">
+        <div class="modal-body">
+
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="saipudin matthew">
+            </div>
+            <div class="mb-3">
+                <label for="nim" class="form-label">NIM</label>
+                <input type="number" class="form-control" id="nim" name="nim" placeholder="saipudin matthew">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="saipudin matthew">
+            </div>
+            <div class="mb-3">
+                <label for="jurusan">Jurusan</label>
+                <select class="form-select" id="jurusan" name="jurusan" aria-label="Default select example">
+                    <option selected>Pilih jurusan</option>
+                    <option value="Teknik Mesin">Teknik Mesin</option>
+                    <option value="Teknik Industri">Teknik Industri</option>
+                    <option value="Teknik Pangan">Teknik Pangan</option>
+                    <option value="Teknik Planologi">Teknik Planologi</option>
+                    <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+                </select>
+            </div>
+
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
