6d8d56274f26eebc32f7d2d3b02d987d475ad62d
Acla Putra <muhamadacla22@gmail.com>
2021-11-16 01:09:23 +0700

    moved database query to database wrapper

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,48 +1,43 @@
 <?php
 
 class Mahasiswa_model {
+    private $table = 'mahasiswa';
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database;
+    }
+
     /** PHP PDO (PHP DATA OBJECT)
      * lebih mudah daripada drivernya mysqli
-     * $dbh (database handler) utk koneksi ke database
-     * $stmt (statement) utk menyimpan query
-     */
-    private $dbh;
-    private $stmt;
-
-    /**
+     *
      * jangan menaruh username dan database kalian di file ini-
      * hrus di simpan di file lain biar ngga diakses sembarang orang
      * $dsn (data source name) utk koneksi ke PDO
      */
-    public function __construct() {
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-        /** cek apakah koneksinya berhasil atau tidak
-         * pangging PDO nya dgn new PDO()
-         * catch ketika error tangkap exception masukkan-
-         * ke dalam variabel $e
-         * die($e->getMessage()); kalau error kasih pesan errornya
-         */
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
 
     // ambil data dari variable $mhs dan kembalikan nilainya dgn return
     public function getAllMahasiswa() {
-        /** this statement diisi dengan handlernya this dbh
-         * jika pakai PDO query harus kita prepare dahulu-
-         * baru masukkan query nya
+        /** panggil db pda construct yg memanggil class Database
+         * panggil function query dan masukkan argumen yg dibutuhkan
+         * return kembalikan nilai pd class database menggunakan function resultSet()
          */
+        $this->db->query('SELECT * FROM '. $this->table);
+        return $this->db->resultSet();
+    }
 
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        // this statement kita execute
-        $this->stmt->execute();
-        /** ambil data dengan mereturn statement dan ambil semua data-
-         * dengan menggunakan fetchAll(), kembalikan data sbagai-
-         * array assosiatif
-         */ 
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function getMahasiswaById($id) {
+        /**
+         * panggil function query dan masukkan argumen yg dibutuhkan
+         * select * from mahasiswa where id = :id utk menyimpan data yg kita binding
+         * id tidak dimasukkan langsung $id utk menghindari sql injection
+         * id dimasukkan melalui function bind($param, $value, $type = null)
+         * panggil function bind 
+         */
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        // id pada WHERE id diisi dengan $id pada argumen getMahasiswaById
+        $this->db->bind('id', $id);
+        // isi cuma satu pakai function single() dripada resultSet() utk all
+        return $this->db->single();
     }
 }
